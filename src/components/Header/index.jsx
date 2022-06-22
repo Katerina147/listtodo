bfe2d5ffaff4a7b8bf34d082d9f0863adcd1a598
Kateryna Chorna <kate.black.vin@gmail.com>
2022-06-22 17:56:39 +0300

    add scss

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import s from './Header.module.css'
 import { Row, Col } from 'react-bootstrap'
+import s from './Header.module.scss'
 
-function Header() {
+export const Header = () => {
     return (
         <Row>
             <Col>
@@ -11,5 +11,3 @@ function Header() {
         </Row>
     )
 }
-
-export default Header
