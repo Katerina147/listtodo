29ba30546ae69f851b87d12bf35a2d990403a5ce
Kateryna Chorna <kate.black.vin@gmail.com>
2022-06-22 16:28:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/Header/Header'
+import AddTodo from './components/Header/AddToDo/AddTodo'
+import TodoList from './components/Header/ToDoList/TodoList'
+import { useState } from 'react'
+import { Container } from 'react-bootstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todo, setTodo] = useState([
+        {
+            id: 1,
+            title: 'Learn React',
+            status: true,
+        },
+        {
+            id: 2,
+            title: 'Learn Redux',
+            status: true,
+        },
+        {
+            id: 3,
+            title: 'Chilling',
+            status: true,
+        },
+    ])
+    return (
+        <Container>
+            <Header />
+            <AddTodo todo={todo} setTodo={setTodo} />
+            <TodoList todo={todo} setTodo={setTodo} />
+        </Container>
+    )
 }
-
-export default App;
+export default App
